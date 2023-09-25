134136394b0969ce6ed81d8b26e166b18486862f
Manuel Dario Oliver Ballesteros <manuel.dario.oliver@gmail.com>
2023-09-25 19:04:26 +0000

    chore: refactor code to a more functional style

--- src/route/game/accept.rs
+++ src/route/game/accept.rs
@@ -4,18 +4,18 @@ use sqlx::{error::ErrorKind, PgPool};
 use tracing::{error, info};
 
 #[tracing::instrument]
-pub async fn handler(State(postgres): State<PgPool>, Json(payload): Json<Accept>) -> StatusCode {
+pub async fn handler(
+    State(postgres): State<PgPool>,
+    Json(payload): Json<Accept>,
+) -> Result<StatusCode, StatusCode> {
     info!("Accepting invite");
 
-    let mut trx = match postgres.begin().await {
-        Ok(x) => x,
-        Err(err) => {
-            error!("Error starting transaction {err}");
-            return StatusCode::INTERNAL_SERVER_ERROR;
-        }
-    };
+    let mut trx = postgres.begin().await.map_err(|err| {
+        error!("Error starting transaction {err}");
+        StatusCode::INTERNAL_SERVER_ERROR
+    })?;
 
-    let res = sqlx::query!(
+    sqlx::query!(
         "
         SELECT true as exists
         FROM games.v_pending_invites
@@ -26,23 +26,14 @@ pub async fn handler(State(postgres): State<PgPool>, Json(payload): Json<Accept>
         payload.invited,
     )
     .fetch_optional(&mut *trx)
-    .await;
+    .await
+    .map_err(|err| {
+        error!("Error checking if this invite exists {err}");
+        StatusCode::INTERNAL_SERVER_ERROR
+    })?
+    .map_or_else(|| Err(StatusCode::NOT_FOUND), |_| Ok(true))?;
 
-    let res = match res {
-        Ok(x) => x,
-        Err(err) => {
-            error!("Error checking if this invite exists {err}");
-            return StatusCode::INTERNAL_SERVER_ERROR;
-        }
-    };
-
-    let exists = res.map_or_else(|| false, |_| true);
-
-    if !exists {
-        return StatusCode::NOT_ACCEPTABLE;
-    }
-
-    let res = sqlx::query!(
+    sqlx::query!(
         "
         DELETE FROM games.v_pending_invites
         WHERE inviter = $1
@@ -52,17 +43,13 @@ pub async fn handler(State(postgres): State<PgPool>, Json(payload): Json<Accept>
         payload.invited,
     )
     .execute(&mut *trx)
-    .await;
+    .await
+    .map_err(|err| {
+        error!("Error deleting invite {err}");
+        StatusCode::INTERNAL_SERVER_ERROR
+    })?;
 
-    match res {
-        Ok(_) => (),
-        Err(err) => {
-            error!("Error deleting invite {err}");
-            return StatusCode::INTERNAL_SERVER_ERROR;
-        }
-    };
-
-    let res = sqlx::query!(
+    sqlx::query!(
         "
         INSERT INTO games.t_active(player_w, player_b, fen, start_pos) 
         VALUES ($1, $2, $3, $3)
@@ -72,31 +59,25 @@ pub async fn handler(State(postgres): State<PgPool>, Json(payload): Json<Accept>
         chess::Board::default().to_string(),
     )
     .execute(&mut *trx)
-    .await;
-
-    match res {
-        Ok(_) => (),
-        Err(err) => {
-            if err
-                .as_database_error()
-                .is_some_and(|err| err.kind() == ErrorKind::UniqueViolation)
-            {
-                return StatusCode::NOT_ACCEPTABLE;
-            } else {
-                error!("Error inserting new game {err}");
-                return StatusCode::INTERNAL_SERVER_ERROR;
-            }
+    .await
+    .map_err(|err| {
+        if err
+            .as_database_error()
+            .is_some_and(|err| err.kind() == ErrorKind::UniqueViolation)
+        {
+            StatusCode::NOT_ACCEPTABLE
+        } else {
+            error!("Error inserting new game {err}");
+            StatusCode::INTERNAL_SERVER_ERROR
         }
-    };
+    })?;
 
-    match trx.commit().await {
-        Ok(_) => (),
-        Err(err) => {
-            error!("Error commiting transaction {err}");
-            return StatusCode::INTERNAL_SERVER_ERROR;
-        }
-    };
-    StatusCode::OK
+    trx.commit().await.map_err(|err| {
+        error!("Error commiting transaction {err}");
+        StatusCode::INTERNAL_SERVER_ERROR
+    })?;
+
+    Ok(StatusCode::OK)
 }
 
 #[derive(Debug, Deserialize)]
